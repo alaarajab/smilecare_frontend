b56b49f4daa59019c6ee15a626c10af05ce230a2
Alaa Rajab <alaa88rajab@gmail.com>
2026-01-22 13:15:50 -0600

    Fix Vite base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/smilecare_frontend/", // ✅ REQUIRED for GitHub Pages
   server: {
     port: 3000,
   },
